767fd5f0be6fc4a0b31de41a83a3915d7e32ccb8
Retro-Coder323 <serkolalozkin@gmail.com>
2026-06-15 12:28:53 +0500

    Новая структура сайта

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const PORT = process.env.PORT || 10879;
+
+
+app.use(express.static('public'));
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+
+let applications = []; 
+
+
+app.listen(PORT, () => {
+    console.log(`Сервер успешно запущен на порту ${PORT}`);
+});
